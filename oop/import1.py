3c80893a3594ec2d3ea487ff364382a74a28b8b2
tatik <tamer.atik@gmail.com>
2018-03-24 01:40:09 +0300

    aa

--- oop/import1.py
+++ oop/import1.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://plusbim:psqlSU@localhost:5432/plusbim"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tamer:psqlSU@localhost:5432/tamer"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -14,13 +14,12 @@ def main():
     r = csv.reader(f)
 
     for origin, destination, duration in r:
-        print("data")
         flight = Flight(origin=origin, destination=destination, duration=duration)
         db.session.add(flight)
         print(f"Added flight form {flight.origin} to {flight.destination} lasting {flight.duration} minutes.")
 
     db.session.commit()
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     with app.app_context():
         main()
