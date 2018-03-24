dbe54f3d759943bbbda8cabc985460360a77451a
tatik <tamer.atik@gmail.com>
2018-03-24 14:06:55 +0300

    updated airline2

--- airline2/application.py
+++ airline2/application.py
@@ -1,18 +1,20 @@
 import os
 
 from flask import Flask, render_template, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tamer:psqlSU@localhost:5432/tamer"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
-engine = create_engine ('postgresql://tamer:psqlSU@localhost:5432/tamer') #os.getenv("DATABASE_URL")
-db = scoped_session(sessionmaker(bind=engine))
+
 
 @app.route("/")
 
 def index():
-    flights = db.execute("SELECT id, origin, destination, duration FROM flights").fetchall()
+    #flights = db.execute("SELECT id, origin, destination, duration FROM flights").fetchall()
+    flights = Flight.query.all()
     return render_template("index.html", flights=flights)
 
 @app.route("/book", methods=["POST"])
@@ -27,17 +29,20 @@ def book():
         return render_template("error.html", message="Invalid flight number.")
 
     # Make sure the flight exists.
-    if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+    flight = Flight.query.get(flight_id)
+
+    if flight is None:
         return render_template("error.html", message="No such flight with that id.")
-    db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
-            {"name": name, "flight_id": flight_id})
-    db.commit()
+    #add passenger
+    passenger = Passenger(name=name, flight_id=flight_id)
+    db.session.add(passenger)
+    db.session.commit()
     return render_template("success.html")
 
 @app.route("/flights")
 def flights():
     """List all flights."""
-    flights = db.execute("SELECT * FROM flights").fetchall()
+    flights = Flight.query.all()
     return render_template("flights.html", flights=flights)
 
 @app.route("/flights/<int:flight_id>")
@@ -45,8 +50,11 @@ def flight(flight_id):
     """Lists details about a single flight."""
 
     #Make sure that flight exists.
-    flight = db.execute("SELECT * from flights WHERE id = :id", {"id": flight_id}).fetchone()
+    flight = Flight.query.get(flight_id)
+    #flight = db.execute("SELECT * from flights WHERE id = :id", {"id": flight_id}).fetchone()
     if flight is None:
         return render_template("error.html", message="No such flight.")
-    passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id", {"flight_id": flight_id}).fetchall()
+
+    #passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id", {"flight_id": flight_id}).fetchall()
+    passengers = Passenger.query.filter_by(flight_id=flight_id).all()
     return render_template("flight.html", flight=flight, passengers=passengers)
