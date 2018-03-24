a453dc566622f200d463c86900812945dab5767f
tatik <tamer.atik@gmail.com>
2018-03-24 19:32:25 +0300

    airline4 , orm usage added

---
+++ airline3/application.py
@@ -0,0 +1,61 @@
+import os
+
+from flask import Flask, render_template, request
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tamer:psqlSU@localhost:5432/tamer"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+
+@app.route("/")
+
+def index():
+    #flights = db.execute("SELECT id, origin, destination, duration FROM flights").fetchall()
+    flights = Flight.query.all()
+    return render_template("index.html", flights=flights)
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight."""
+
+    # Get form information.
+    name = request.form.get("name")
+    try:
+        flight_id = request.form.get("flight_id")
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    # Make sure the flight exists.
+    flight = Flight.query.get(flight_id)
+
+    if flight is None:
+        return render_template("error.html", message="No such flight with that id.")
+    #add passenger
+    #instead of passenger = Passenger(name=name, flight_id=flight_id)
+    #db.session.add(passenger)
+    #db.session.commit(), using only
+    flight.add_passenger(name)
+    return render_template("success.html")
+
+@app.route("/flights")
+def flights():
+    """List all flights."""
+    flights = Flight.query.all()
+    return render_template("flights.html", flights=flights)
+
+@app.route("/flights/<int:flight_id>")
+def flight(flight_id):
+    """Lists details about a single flight."""
+
+    #Make sure that flight exists.
+    flight = Flight.query.get(flight_id)
+    #flight = db.execute("SELECT * from flights WHERE id = :id", {"id": flight_id}).fetchone()
+    if flight is None:
+        return render_template("error.html", message="No such flight.")
+
+    #passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id", {"flight_id": flight_id}).fetchall()
+    passengers = Passenger.query.filter_by(flight_id=flight_id).all()
+    return render_template("flight.html", flight=flight, passengers=passengers)
